b31ad3a371e8fb56fb4ed502a37fdf837b2e577e
Jake Israel <jcmisrael@gmail.com>
2015-09-15 03:52:11 -0400

    Initial creation of unit tests for engine/containers/dynamic_array; NONE WRITTEN YET

---
+++ test/engine/containers/dynamic_array_test.cpp
@@ -0,0 +1,15 @@
+// dynamic_array_test.cpp
+// Unit tests for DynamicArray
+
+#include "dynamic_array.hpp"
+#include "gtest/gtest.h"
+
+namespace {
+
+using StevensDev::sgdc::DynamicArray;
+
+TEST(DynamicArrayTest, simple){
+    ASSERT_EQ(1,1);
+}
+
+} // End anonymous namespace
